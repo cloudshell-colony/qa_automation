318bbd92f132e2304dbbc2baa8e738303b6e1312
asafyahalom <asaf.y@quali.com>
2022-06-19 15:37:47 +0300

    updated Spaces flow

--- tests/functions/Spaces/goToSpace.mjs
+++
@@ -1,5 +0,0 @@
-import {clickButton} from '../selectors.mjs';
-export default async (page, spaceName) => {
-    await page.click("div[data-test=sidebar-dropdown]");
-    await clickButton(page, `option__${spaceName}`);
-};
--- tests/functions/goToAdministration.mjs
+++
@@ -1,5 +0,0 @@
-const {clickButton} = require("./selectors.mjs");
-export default async (page) => {
-    await page.click("div[data-test=sidebar-dropdown]");
-    await clickButton(page, "option__admin");
-};
