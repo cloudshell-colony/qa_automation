837fc455b4f7e0725fc8dc11f74bd41d430f6c38
giladmp1978 <gilad.m@quali.com>
2022-06-21 11:08:04 +0300

    update to new data-test

--- tests/functions/goToAdminConsole.mjs
+++ tests/functions/goToAdminConsole.mjs
@@ -1,5 +1,6 @@
 export default async (page) => {
-    await page.click("div[data-test=sidebar-dropdown]");
+    await page.pause();
+    await page.click('[data-test="currently-selected-space"]');
     await page.click("[data-test=option__admin]");
     await page.click("[data-test=space-tab]");
     await page.waitForSelector('[role=rowgroup]');
