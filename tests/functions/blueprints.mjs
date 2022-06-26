6aa7f5d51ae8164863d7fc05c093c3a412fc491c
asafyahalom <asaf.y@quali.com>
2022-06-26 17:20:29 +0300

    added function to count blueprints

--- tests/functions/blueprints.mjs
+++ tests/functions/blueprints.mjs
@@ -1,6 +1,8 @@
 import fetch from "node-fetch";
+import {goToSpace} from "./spaces.mjs";
 
-const getPublishedBlueprints = async (session, space_name, myURL) => {
+
+export const getPublishedBlueprints = async (session, space_name, myURL) => {
     
     const PBPList = await fetch(`${myURL}/api/spaces/${space_name}/blueprints`, {
         "method": "GET",
@@ -12,4 +14,18 @@ const getPublishedBlueprints = async (session, space_name, myURL) => {
     return(PBPList);    
 };
 
-export default getPublishedBlueprints;
+export const countBlueprintsInSpace = async(page, space_name) => {
+    await goToSpace(page, space_name);
+    await page.click("[data-test=blueprints-nav-link]");
+    await page.waitForSelector("[data-test=space-blueprints-tab-test]");
+    const visi = await page.isVisible('button:has-text("Add a Repository")', {timeout: 3000});
+    if(visi){
+        return 0;
+    }
+    else{
+        await page.waitForSelector('div:has-text("Launch Sandbox")', {timeout: 3000});
+        const BPList = page.locator("tr", {has: page.locator("data-testid=moreMenu")});
+        const num = await BPList.count();
+        return num;
+    }
+};
