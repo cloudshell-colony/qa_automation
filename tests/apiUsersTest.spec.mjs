49b0111ed670edb26b46bcd828cfb9146a3902a8
giladmp1978 <gilad.m@quali.com>
2022-07-05 08:36:47 +0300

    formatting

--- tests/apiUsersTest.spec.mjs
+++ tests/apiUsersTest.spec.mjs
@@ -10,10 +10,10 @@ the user cannot obtain a new session
 */
 
 import test, { expect } from "@playwright/test";
-import {generateSecret} from './functions/general.mjs';
-import {signupUserAPI, getSessionAPI, sendInvitationsAPI, getInvitationAPI, deleteUserAPI} from "./functions/accounts.mjs";
+import { generateSecret } from './functions/general.mjs';
+import { signupUserAPI, getSessionAPI, sendInvitationsAPI, getInvitationAPI, deleteUserAPI } from "./functions/accounts.mjs";
 import getAllUsersAPI from "./functions/users.mjs";
-import getPublishedBlueprints from "./functions/blueprints.mjs";
+import { getPublishedBlueprints } from "./functions/blueprints.mjs";
 
 const baseURL = process.env.baseURL;
 const PASSWORD = process.env.allAccountsPassword;
@@ -31,10 +31,10 @@ const newUserEmail = "marvel10@dc.com";
 const secret = generateSecret(newUserEmail, account);
 
 test.describe.serial('basic API tests', () => {
-    
+
     test('get all users', async () => {
         const usersList = await getAllUsersAPI(session, baseURL);
-       
+
         const usersListJson = await usersList.json()
         if (usersList.status === 200) {
             console.log(await usersListJson);
@@ -45,58 +45,58 @@ test.describe.serial('basic API tests', () => {
             expect(usersList.status).toBe(200);
             expect(usersList.ok).toBeTruthy();
         }
-                
+
     });
-   
-    test('invite new user', async () => {        
-        const userInvitation = await sendInvitationsAPI(session, newUserEmail, baseURL, space)        
-        if (userInvitation.status === 200 ) {
-            expect(userInvitation.ok).toBeTruthy(); 
-            console.log(`invitation to ${newUserEmail} was submited`);                
-        } else {            
+
+    test('invite new user', async () => {
+        const userInvitation = await sendInvitationsAPI(session, newUserEmail, baseURL, space)
+        if (userInvitation.status === 200) {
+            expect(userInvitation.ok).toBeTruthy();
+            console.log(`invitation to ${newUserEmail} was submited`);
+        } else {
             console.log(await userInvitation.json());
             expect(userInvitation.status).toBe(200);
         }
-        
+
     });
-    
-    test('get invitation by secret', async () => {             
+
+    test('get invitation by secret', async () => {
         const invitationInfo = await getInvitationAPI(baseURL, secret);
-        if (invitationInfo.status === 200 ) {
-            expect(invitationInfo.ok).toBeTruthy();   
+        if (invitationInfo.status === 200) {
+            expect(invitationInfo.ok).toBeTruthy();
             console.log(await invitationInfo.json());
         } else {
             console.log(await invitationInfo.json());
-            expect(invitationInfo.status).toBe(200);    
+            expect(invitationInfo.status).toBe(200);
         };
-        
+
     });
-    
+
     test("user signup by useing his secret", async () => {
-             
+
         const signupUserAPIrequest = await signupUserAPI(baseURL, secret);
-        if (signupUserAPIrequest.status != 200 ) {
+        if (signupUserAPIrequest.status != 200) {
             console.log(await signupUserAPIrequest.json());
             expect(signupUserAPIrequest.status).toBe(200);
-            expect(signupUserAPIrequest.ok).toBeTruthy();     
+            expect(signupUserAPIrequest.ok).toBeTruthy();
         } else {
             expect(signupUserAPIrequest.status).toBe(200);
             expect(signupUserAPIrequest.ok).toBeTruthy();
         }
-        
+
     });
 
-    test('user invitation by secret is no longer available after the user registrated', async () => {             
+    test('user invitation by secret is no longer available after the user registrated', async () => {
         const invitationInfo = await getInvitationAPI(baseURL, secret);
         const body = await invitationInfo.json()
-        if (invitationInfo.status === 404 ) {
+        if (invitationInfo.status === 404) {
             expect(invitationInfo.status).toBe(404);
             console.log(await body);
-            expect(body.errors[0].message).toBe('Invitation was not found');            
+            expect(body.errors[0].message).toBe('Invitation was not found');
         } else {
             console.log(await body);
-            expect(invitationInfo.status).toBe(404);    
-        };        
+            expect(invitationInfo.status).toBe(404);
+        };
     });
 
     test('get all users again', async () => {
@@ -112,15 +112,15 @@ test.describe.serial('basic API tests', () => {
 
     test('new user can get a session', async () => {
         newUserSession = await getSessionAPI(newUserEmail, PASSWORD, baseURL, account);
-        if (typeof(newUserSession) === "object") {
+        if (typeof (newUserSession) === "object") {
             console.log(await newUserSession.json())
-            expect(typeof(newUserSession)).toBe("string");    
+            expect(typeof (newUserSession)).toBe("string");
         } else {
-        expect(typeof(newUserSession)).toBe("string");
-        console.log(`email: ${newUserEmail},   session: ${newUserSession}`);
+            expect(typeof (newUserSession)).toBe("string");
+            console.log(`email: ${newUserEmail},   session: ${newUserSession}`);
         };
     });
-    
+
     test('New user can get all published blueprints', async () => {
         const publishedBlueprints = await getPublishedBlueprints(newUserSession, space, baseURL);
         expect(publishedBlueprints.status).toBe(200);
@@ -131,15 +131,15 @@ test.describe.serial('basic API tests', () => {
 
     test('Admin can delete a user from the system', async () => {
         const deleteUserAPIResponse = await deleteUserAPI(newUserEmail, baseURL, session);
-        if (deleteUserAPIResponse.status != 200 ) {
+        if (deleteUserAPIResponse.status != 200) {
             console.log(`my log text should be: ${await deleteUserAPIResponse.text()}`);
             expect(deleteUserAPIResponse.status).toBe(200);
-            expect(deleteUserAPIResponse.ok).toBeTruthy();     
-        } else {            
+            expect(deleteUserAPIResponse.ok).toBeTruthy();
+        } else {
             expect(deleteUserAPIResponse.status).toBe(200);
             expect(deleteUserAPIResponse.ok).toBeTruthy();
         }
-        
+
     });
 
     test('Deleted user should NOT be able to use his active session', async () => {
@@ -152,15 +152,15 @@ test.describe.serial('basic API tests', () => {
 
     test('Deleted user cannot obtain a session', async () => {
         newUserSession = await getSessionAPI(newUserEmail, PASSWORD, baseURL, account);
-        if (typeof(newUserSession) === "object") {
+        if (typeof (newUserSession) === "object") {
             const response = await newUserSession.json();
             console.log(await response);
             expect(newUserSession.status).toBe(401);
-            expect(response.errors[0].message).toBe("The email address or password is incorrect");    
+            expect(response.errors[0].message).toBe("The email address or password is incorrect");
         } else {
-        console.log("Deleted user was able to get active session");
-        expect(typeof(newUserSession)).toBe("object");
-        expect(newUserSession.status).toBe(401);
+            console.log("Deleted user was able to get active session");
+            expect(typeof (newUserSession)).toBe("object");
+            expect(newUserSession.status).toBe(401);
         };
     })
 
