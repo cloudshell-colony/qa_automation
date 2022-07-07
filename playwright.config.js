6fe233aedb3880633e4ab3b60cb1b5fd8d0edf87
amiros89 <amir.rashko@gmail.com>
2022-07-07 16:46:16 +0300

    change reporter type to xml

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,7 @@ require('dotenv').config();
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
+  testDir: './tests',
   /* Maximum time one test can run for. */
   timeout: 5 * 60 * 1000,
   expect: {
@@ -31,7 +32,7 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [ ['junit', { outputFile: 'results.xml' }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
