43e7bf35ca5ce7d3f85bc966aed68232e0af6230
giladmp1978 <gilad.m@quali.com>
2022-07-17 20:45:00 +0300

    api user tests

--- tests/apiUsersTest.spec.mjs
+++ tests/apiUsersTest.spec.mjs
@@ -20,19 +20,22 @@ const PASSWORD = process.env.allAccountsPassword;
 const account = process.env.account;
 const USER = process.env.adminEMail
 const space = process.env.space
+const timestemp = Math.floor(Date.now() / 10000);
 
 let initialNumberOfUsersInDomain = 0;
 let newNumberOfUsersInDomain = 0;
 let newUserSession = "";
 
-const session = await getSessionAPI(USER, PASSWORD, baseURL, account);
 // new user data
-const newUserEmail = "marvel10@dc.com";
+const newUserEmail = ("qaAuto").concat(timestemp).concat("10@dc.com");
 const secret = generateSecret(newUserEmail, account);
 
-test.describe.serial('basic API tests', () => {
+// get admin session
+const session = await getSessionAPI(USER, PASSWORD, baseURL, account);
 
-    test('get all users', async () => {
+test.describe.serial('Add and delete user', () => {
+
+    test('Get all users', async () => {
         const usersList = await getAllUsersAPI(session, baseURL);
 
         const usersListJson = await usersList.json()
@@ -48,7 +51,7 @@ test.describe.serial('basic API tests', () => {
 
     });
 
-    test('invite new user', async () => {
+    test('Invite new user', async () => {
         const userInvitation = await sendInvitationsAPI(session, newUserEmail, baseURL, space)
         if (userInvitation.status === 200) {
             expect(userInvitation.ok).toBeTruthy();
@@ -60,7 +63,7 @@ test.describe.serial('basic API tests', () => {
 
     });
 
-    test('get invitation by secret', async () => {
+    test('Get invitation by secret', async () => {
         const invitationInfo = await getInvitationAPI(baseURL, secret);
         if (invitationInfo.status === 200) {
             expect(invitationInfo.ok).toBeTruthy();
@@ -72,7 +75,7 @@ test.describe.serial('basic API tests', () => {
 
     });
 
-    test("user signup by useing his secret", async () => {
+    test("User signup by useing his secret", async () => {
 
         const signupUserAPIrequest = await signupUserAPI(baseURL, secret);
         if (signupUserAPIrequest.status != 200) {
@@ -84,13 +87,26 @@ test.describe.serial('basic API tests', () => {
             expect(signupUserAPIrequest.ok).toBeTruthy();
         }
 
+    }); test("User can only signup once useing his secret", async () => {
+
+        const signupUserAPIrequest = await signupUserAPI(baseURL, secret);
+        if (signupUserAPIrequest.status != 404) {
+            console.log(await signupUserAPIrequest.json());
+            expect(signupUserAPIrequest.status).toBe(404);
+        } else {
+            console.log('user can signup only once');
+            console.log(await signupUserAPIrequest.json());
+            expect(signupUserAPIrequest.status).toBe(404);
+        }
+
     });
 
-    test('user invitation by secret is no longer available after the user registrated', async () => {
+    test('User invitation by secret is no longer available after the user registrated', async () => {
         const invitationInfo = await getInvitationAPI(baseURL, secret);
         const body = await invitationInfo.json()
         if (invitationInfo.status === 404) {
             expect(invitationInfo.status).toBe(404);
+            console.log('As expected dseleted user cannot obtain a session');
             console.log(await body);
             expect(body.errors[0].message).toBe('Invitation was not found');
         } else {
@@ -99,7 +115,7 @@ test.describe.serial('basic API tests', () => {
         };
     });
 
-    test('get all users again', async () => {
+    test('Get all users again', async () => {
         const usersList = await getAllUsersAPI(session, baseURL);
         expect(usersList.status).toBe(200);
         expect(usersList.ok).toBeTruthy();
@@ -110,7 +126,7 @@ test.describe.serial('basic API tests', () => {
         expect(newNumberOfUsersInDomain - initialNumberOfUsersInDomain).toEqual(1);
     });
 
-    test('new user can get a session', async () => {
+    test('New user can get a session', async () => {
         newUserSession = await getSessionAPI(newUserEmail, PASSWORD, baseURL, account);
         if (typeof (newUserSession) === "object") {
             console.log(await newUserSession.json())
@@ -165,6 +181,3 @@ test.describe.serial('basic API tests', () => {
     })
 
 });
-
-
-
