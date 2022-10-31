a11b2ce22d7aeb2c612e7f20a0f70dedb73c57da
giladmp1978 <gilad.m@quali.com>
2022-10-31 20:41:39 +0200

    api flow

--- tests/onBoardingAPI.spec.mjs
+++ tests/onBoardingAPI.spec.mjs
@@ -51,6 +51,17 @@ test.describe.serial('On boarding with APIs', () => {
             "branch": null,
             "repository_name": "qa_automation102"
         }
+
+        const data2 = {
+            "repository_url": "https://github.com/gilad030609/repo",
+            "access_token": "293fe960eac4f9e031ed",
+            "repository_type": "github",
+            "branch": "master",
+            "type": "sandbox",
+            "repository_name": "repo",
+            "provider_id": repository_provider_id,
+        }
+
         await fetch(`${baseURL}/api/spaces/${spaceName}/repositories/github`, {
             "method": "POST",
             "headers": {
@@ -70,7 +81,7 @@ test.describe.serial('On boarding with APIs', () => {
     });
 
     test('Add execution host to space', async () => {
-
+        await page.pause();
     });
 
 });
