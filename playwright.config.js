dbdbcdca2ec993c5a408214ec7ac035bad8f4995
Amir Omazgin <amir.o@quali.com>
2023-03-19 17:36:03 +0200

    cancel testmatch

--- playwright.config.js
+++ playwright.config.js
@@ -52,7 +52,7 @@ const config = {
     // /*
     {
       name: 'chromium',
-      testMatch: /.policiesTestAWS.spec.mjs/,
+      // testMatch: /.policiesTestAWS.spec.mjs/,
       use: {
         ...devices['Desktop Chrome'],
       },
