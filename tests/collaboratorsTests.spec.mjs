ffea9902e03b14ddabf1c78003004983a807af49
Amir Omazgin <amir.o@quali.com>
2023-03-27 15:26:15 +0300

    added env vars

--- tests/collaboratorsTests.spec.mjs
+++ tests/collaboratorsTests.spec.mjs
@@ -19,11 +19,15 @@ const bpName = 'simpleTF';
 const executionHostName = 'qa-eks';
 const spaceMember = 'asaf.y@quali.com';
 const spaceMemberPassword = 'Qual!123';
-let inputs = {'inputs\.agent': executionHostName};
+let inputs = { 'inputs\.agent': executionHostName };
 let page;
 let session;
 let sandboxName;
-
+let response;
+let responseJson;
+let firstSB;
+let ID;
+let collabInfo;
 /** Test prerequisites
  * Have account with credentials as saved in .env file
  * @space should exist in the account with @executionHostName associated to the space
@@ -33,7 +37,7 @@ let sandboxName;
  */
 
 test.describe('sendbox launch with collab', () => {
-   
+
     test.beforeAll(async ({ browser }) => {
         session = await getSessionAPI(user, password, baseURL, account);
         await validateGetSessionAPI(session);
@@ -47,35 +51,30 @@ test.describe('sendbox launch with collab', () => {
     //     await stopAndValidateAllSBsCompletedAPI(session, baseURL, space);
     // })
 
-    test.skip("launch blueprint with collaborator and validate collaborator name in Sendbox details", async () => {
+    test("launch blueprint with collaborator and validate collaborator name in Sendbox details", async () => {
         await goToSpace(page, space)
         const blueprint = await page.locator(`[data-test="catalog-bp-autogen_${bpName}"]`)
         await blueprint.locator('[data-test="launch-environment-from-blueprint"]').click()
         const collaborator = await page.locator('.hFDyZZ')
-        await(collaborator.locator('.btn-content')).click()
+        await (collaborator.locator('.btn-content')).click()
         await page.locator('.select-set_collab__control').click()
         await page.keyboard.press("Enter");
-        await expect (page.locator('[data-test="selected-collaborators-container"]')).toContainText(collaboratorName)
+        await expect(page.locator('[data-test="selected-collaborators-container"]')).toContainText(collaboratorName)
         await page.locator('[data-test="go-to-next-step"]').click()
-        // await page.locator('[data-test="inputs.ami"]').type('ami-0cd01c7fb16a9b497')
-        selectFromDropbox(page, 'inputs.agent', executionHostName )
+        selectFromDropbox(page, 'inputs.agent', executionHostName)
         await page.locator('[ data-test="launch-environment"]').click()
         await page.locator('[data-test="sandboxes-nav-link"]').click()
         await expect(page.locator('[data-test="sandbox-row-0"]')).toContainText('Launching', { timeout: 4000 });
         await expect(page.locator('[data-test="sandbox-row-0"]')).toContainText('Active', { timeout: 5 * 60 * 1000 });
-        const response = await getFirstSandboxesAPI(session, baseURL, space, count)
-        const responseJson = await response.json()
+        response = await getFirstSandboxesAPI(session, baseURL, space, count)
+        responseJson = await response.json()
         console.log(responseJson)
-        const firstSB = await responseJson[0]
-        // const ID = await firstSB.id
-        // const sandboxId = await findSandboxIdByNameAPI(session, baseURL, space, sandboxName);
-        // console.log(sandboxId);
-
-
-        // console.log("the id is " + ID)
-        const collabInfo = await firstSB.collaborators_info.collaborators
+        firstSB = await responseJson[0]
+        ID = await firstSB.id
+        console.log("the id is " + ID)
+        collabInfo = await firstSB.collaborators_info.collaborators
         console.log(collabInfo[0].email)
-       
+
         try {
             expect(await collabInfo[0].email).toContain(collaboratorName)
             await endSandboxAPI(session, baseURL, space, ID)
@@ -86,10 +85,10 @@ test.describe('sendbox launch with collab', () => {
             test.fail()
         }
         await expect(page.locator('[data-test="sandbox-row-0"]')).toContainText('Terminating', { timeout: 10000 });
-      
+
     });
 
-    test.skip("Adding & removing collaborator to existing environment", async()=>{
+    test("Adding & removing collaborator to existing environment", async () => {
         await goToSpace(page, space);
         sandboxName = await launchBlueprintFromCatalogPage(page, bpName, inputs);
         await page.waitForSelector('[data-test="sandbox-info-column"] div:has-text("StatusActive")', { timeout: 5 * 60 * 1000 });
