c2a0fe3835cabc6d50538fcd2e986962f16cd02e
Amir-Omazgin <118269891+Amir-Omazgin@users.noreply.github.com>
2023-10-02 16:17:25 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -65,6 +65,7 @@ resource "aws_iam_user_policy_attachment" "attachment" {
     policy_arn = aws_iam_policy.policy[0].arn
 }
 
-output "s3_bucket_name" {
+output "created_bucket_name" {
   value = aws_s3_bucket.bucket.name
 }
+
