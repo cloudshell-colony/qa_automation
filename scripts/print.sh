f72cc98df947e1104577a6097afa69f246c66c68
Amir-Omazgin <118269891+Amir-Omazgin@users.noreply.github.com>
2023-10-29 15:07:06 +0200

    Update print.sh

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,3 +1,3 @@
 # echo "2- plan path is: $TORQUE_TF_PLAN_PATH"
-$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1
+echo "$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1"
 
