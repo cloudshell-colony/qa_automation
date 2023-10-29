0123522d8783e3bfe6b56cc927aee12017b22b0a
Amir-Omazgin <118269891+Amir-Omazgin@users.noreply.github.com>
2023-10-29 17:04:32 +0200

    Update print.sh

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,12 +1,6 @@
 # echo "2- plan path is: $TORQUE_TF_PLAN_PATH"
-# echo "$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1"
+echo "$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1"
 
-if grep -q 'Plan: [1-9][0-9]* to add, 0 to change, 0 to destroy' "$TORQUE_TF_PLAN_PATH"; then
-  echo "Error: The plan indicates new resources"
-  exit 1
-else
-  echo "Success: No new resources added."
-  exit 0
 
 
 
