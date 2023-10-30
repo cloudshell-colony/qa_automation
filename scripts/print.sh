7cf3c181b4220cad6e07654e36a549d6f9adc707
Amir-Omazgin <118269891+Amir-Omazgin@users.noreply.github.com>
2023-10-30 09:28:53 +0200

    Update print.sh

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,17 +1,17 @@
 # echo "2- plan path is: $TORQUE_TF_PLAN_PATH"
-# echo "$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1"
-#!/bin/bash
+echo "$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1"
+# #!/bin/bash
 
-# Define some variables
-first_name="John"
-last_name="Doe"
-age=30
+# # Define some variables
+# first_name="John"
+# last_name="Doe"
+# age=30
 
-# Construct a command using variables
-command="echo My name is $first_name $last_name, and I am $age years old."
+# # Construct a command using variables
+# command="echo My name is $first_name $last_name, and I am $age years old."
 
-# Execute the command
-$command
+# # Execute the command
+# $command
 
 
 
