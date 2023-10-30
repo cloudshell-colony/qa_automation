8ed1100be693e2035ff2f989710456e545f82beb
Amir-Omazgin <118269891+Amir-Omazgin@users.noreply.github.com>
2023-10-30 10:15:20 +0200

    Update print.sh

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,4 +1,4 @@
-# echo "2- plan path is: $TORQUE_TF_PLAN_PATH"
+echo "2- plan path is: $TORQUE_TF_PLAN_PATH"
 # echo "$TORQUE_TF_EXECUTABLE -chdir=$TORQUE_TF_MODULE_PATH state rm $1"
 # #!/bin/bash
 
@@ -15,16 +15,16 @@
 
 #!/bin/bash
 
-expected_agent="qa-eks"
-input_agent="{{ .inputs.agent }}"
+# expected_agent="qa-eks"
+# input_agent="{{ .inputs.agent }}"
 
-if [ "$input_agent" != "$expected_agent" ]; then
-  echo "{{ .inputs.agent }}"
-  echo "Error: Input agent does not match the expected value ('$expected_agent')."
-  exit 1
-fi
+# if [ "$input_agent" != "$expected_agent" ]; then
+#   echo "{{ .inputs.agent }}"
+#   echo "Error: Input agent does not match the expected value ('$expected_agent')."
+#   exit 1
+# fi
 
-echo "Input agent is valid: '$input_agent'"
+# echo "Input agent is valid: '$input_agent'"
 
 
 
