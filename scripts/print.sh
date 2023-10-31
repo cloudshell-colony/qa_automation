5638cc6d914d7664bd996c31b66a9781452a78fc
Amir-Omazgin <118269891+Amir-Omazgin@users.noreply.github.com>
2023-10-31 10:42:40 +0200

    Update print.sh

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+# #!/bin/bash
 
-expected_path="/workspace/terraform.plan"
-path="$TORQUE_TF_PLAN_PATH"
+# expected_path="/workspace/terraform.plan"
+# path="$TORQUE_TF_PLAN_PATH"
 
-echo $path
+# echo $path
 
-if [[ "$path" == *"$expected_path"* ]]; then
-  echo "path is valid: '$path'"
-else
-  echo "Error: path ('$path') is not a part of the expected path ('$expected_path')."
-  exit 1
-fi
+# if [[ "$path" == *"$expected_path"* ]]; then
+#   echo "path is valid: '$path'"
+# else
+#   echo "Error: path ('$path') is not a part of the expected path ('$expected_path')."
+#   exit 1
+# fi
 
 
 
@@ -35,14 +35,15 @@ fi
 
 # !/bin/bash
 
-# expected_agent="qa-eks"
-# input_agent="{{ .inputs.agent }}"
-
-# if [ "$input_agent" != "$expected_agent" ]; then
-#   echo "{{ .inputs.agent }}"
-#   echo "Error: Input agent does not match the expected value ('$expected_agent')."
-#   exit 1
-# fi
+expected_agent="qa-eks"
+input_agent="$1"
+echo "$1"
+if [ "$input_agent" != "$expected_agent" ]; then
+  echo "Error: Input agent does not match the expected value ('$expected_agent')."
+  exit 1
+else
+  echo "agent exist"
+fi
 
 # echo "Input agent is valid: '$input_agent'"
 
