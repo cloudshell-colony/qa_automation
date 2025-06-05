e3d83e0bef6fd5ef5fc60accf12e53d4f1532468
johnathanvidu <johnathan.v@qualisystems.com>
2025-06-05 22:26:53 +0300

    Update variables.tf

--- terraform/create-ec2-instance/variables.tf
+++ terraform/create-ec2-instance/variables.tf
@@ -1,7 +1,7 @@
 variable "instance_type" {
   description = "(Optional) The instance type to use for the instance. defaults to t3.micro"
   type        = string
-  default     = "t3.micro"
+  default     = "t4g.nano"
 }
 
 variable "ami" {
